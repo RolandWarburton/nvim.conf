cfc27fb06960f4136e9d8a1f1835380382e8195d
RolandWarburton <warburtonroland@gmail.com>
2022-07-03 18:21:51 +1000

    Clean up old code

--- lua/keybindings/comment.lua
+++ lua/keybindings/comment.lua
@@ -3,7 +3,6 @@
 
 -- Linewise toggle current line using C-/
 vim.keymap.set('n', '<C-_>', ':lua require("Comment.api").toggle_current_linewise()<CR>')
--- map('n', 'C-_', ':lua ToggleComment()<cr>' )
 
 -- Linewise toggle using C-/
 vim.keymap.set('x', '<C-_>', '<ESC><CMD>lua require("Comment.api").toggle_linewise_op(vim.fn.visualmode())<CR>')
