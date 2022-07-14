f8edc31e49de6590242708280f0592e83fe703cf
RolandWarburton <warburtonroland@gmail.com>
2022-07-14 17:21:53 +1000

    Update DAP config

--- lua/extensions/nvimDap.lua
+++ lua/extensions/nvimDap.lua
@@ -6,19 +6,30 @@ dap.adapters.node2 = {
   type = "executable",
   command = "node",
   args = {
-    "/home/roland/Documents/projects/vscode-node-debug2/out/src/nodeDebug.js",
+    "/home/roland/.local/debug/vscode-node-debug2/out/src/nodeDebug.js",
   },
 }
 
 dap.configurations.javascript = {
+  -- this doesn't work that great (or at all)
   {
-    type = "node2",
-    request = "launch",
-    program = "${file}",
+    name = 'Launch',
+    type = 'node2',
+    request = 'launch',
+    -- program = '${file}',
     cwd = vim.fn.getcwd(),
-    sourceMaps = true,
-    protocol = "inspector",
-    console = "integratedTerminal",
+    -- sourceMaps = true,
+    protocol = 'inspector',
+    console = 'integratedTerminal',
+  },
+  -- this works at the moment if you manually start the built javascript
+  -- For this to work you need to make sure the node process is started with the `--inspect` flag.
+  {
+    name = 'Attach to process',
+    type = 'node2',
+    request = 'attach',
+    processId = require'dap.utils'.pick_process,
   },
 }
 
+dap.configurations.typescript = dap.configurations.javascript
