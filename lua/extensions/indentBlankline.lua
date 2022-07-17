bede846f3bc76c3d403fb8ffc9cf7070e0fcca48
RolandWarburton <warburtonroland@gmail.com>
2022-07-17 19:31:17 +1000

    Update indent blankline config

--- lua/extensions/indentBlankline.lua
+++ lua/extensions/indentBlankline.lua
@@ -12,6 +12,7 @@ vim.api.nvim_set_hl(0, "IndentBlankLineIndent3", {default = false, fg = "#179CFB
 
 require("indent_blankline").setup({
   space_char_blankline = " ",
+  show_trailing_blankline_indent = false,
   show_current_context = true,
   show_current_context_start = false,
   space_char_highlight_list = {
