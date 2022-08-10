fe84b0cb24bde152c5dbaaede777d76bf8fe587f
RolandWarburton <warburtonroland@gmail.com>
2022-08-10 19:50:55 +1000

    Markdown spell sitter

--- lua/extensions/spellSitter.lua
+++ lua/extensions/spellSitter.lua
@@ -9,7 +9,7 @@ local spellsitter_augroup = vim.api.nvim_create_augroup("spellsitter_augroup", {
 -- disable spellchecking for markdown
 -- You can determine the file type using the `:echo &filetype` command
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "markdown", "NeogitStatus" },
+  pattern = { "NeogitStatus" },
   command = "setlocal nospell",
   group = spellsitter_augroup,
 })
