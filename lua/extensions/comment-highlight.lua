dbf798058b039aaae9217e2b62997ee0f7a9cc6d
RolandWarburton <warburtonroland@gmail.com>
2022-09-05 18:48:07 +1000

    Add comment highlighting

---
+++ lua/extensions/comment-highlight.lua
@@ -0,0 +1,34 @@
+require('better-comment').Setup(
+  {
+    tags = {
+      {
+        name = "TODO",
+        fg = "white",
+        bg = "#0a7aca",
+        bold = true,
+        virtual_text = "",
+      },
+      {
+        name = "FIX",
+        fg = "white",
+        bg = "#f44747",
+        bold = true,
+        virtual_text = "",
+      },
+      {
+        name = "WARNING",
+        fg = "#FFA500",
+        bg = "",
+        bold = false,
+        virtual_text = "",
+      },
+      {
+        name = "!",
+        fg = "#f44747",
+        bg = "",
+        bold = true,
+        virtual_text = "",
+      }
+    }
+  }
+)
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -90,6 +90,7 @@ return packer.startup(function(use, use_rocks)
   use { "andweeb/presence.nvim" }
   use { "jbyuki/venn.nvim" }
   use { 'anuvyklack/hydra.nvim' }
+  use { 'Djancyp/better-comments.nvim' }
   use_rocks { 'inifile' }
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
