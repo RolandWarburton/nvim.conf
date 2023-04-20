acd7b5a537b1aa8214c32bf21fe399cf25e88744
RolandWarburton <warburtonroland@gmail.com>
2023-04-20 21:10:58 +1000

    Update vscode theme

--- init.lua
+++ init.lua
@@ -1,11 +1,19 @@
 local home = require'os'.getenv('HOME')
 
 -- color scheme
+vim.o.background = 'dark'
 local ok, vscode = pcall(require, 'vscode')
 if ok then
   vscode.setup({
     transparent = false,
+    italic_comments = true,
+    disable_nvimtree_bg = true,
+    -- Override colors (see ./lua/vscode/colors.lua)
+    color_overrides = {
+      vscLineNumber = '#FFFFFF',
+    },
   })
+  require('vscode').load()
 end
 
 local ini = require 'inifile'
