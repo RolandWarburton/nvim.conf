6bc977e4c25d3c4446c4987551e3381758808303
RolandWarburton <warburtonroland@gmail.com>
2023-10-20 14:47:21 +1100

    Update illuminate providers list

--- lua/extensions/illuminate.lua
+++ lua/extensions/illuminate.lua
@@ -1,4 +1,9 @@
 require('illuminate').configure({
+  -- providers: provider used to get references in the buffer, ordered by priority
+  providers = {
+    'lsp',
+    'treesitter',
+  },
   -- do not highlight words in non editable buffers
   should_enable = function(bufnr) return vim.bo.modifiable and true or false end,
 })
