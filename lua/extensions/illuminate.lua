453300ac6fa08c7975d07a55b1449680a1872e66
RolandWarburton <warburtonroland@gmail.com>
2023-10-20 14:40:45 +1100

    Do not illuminate in nvim tree

--- lua/extensions/illuminate.lua
+++ lua/extensions/illuminate.lua
@@ -1,1 +1,4 @@
-require('illuminate').configure({})
+require('illuminate').configure({
+  -- do not highlight words in non editable buffers
+  should_enable = function(bufnr) return vim.bo.modifiable and true or false end,
+})
