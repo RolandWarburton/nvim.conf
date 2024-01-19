204de65cba4086b9054ea1efe1b86bf7da20fe37
RolandWarburton <warburtonroland@gmail.com>
2024-01-19 17:45:40 +1100

    Add LSP gutter signs

--- lua/lsp/lsp.lua
+++ lua/lsp/lsp.lua
@@ -82,3 +82,27 @@ require 'lspconfig'.lua_ls.setup {
     },
   },
 }
+
+-- gutter signs for LSP errors
+local sign = function(opts)
+  vim.fn.sign_define(opts.name, {
+    texthl = opts.name,
+    text = opts.text,
+    numhl = ''
+  })
+end
+
+sign({ name = 'DiagnosticSignError', text = 'x' })
+sign({ name = 'DiagnosticSignWarn', text = '▲' })
+sign({ name = 'DiagnosticSignHint', text = '⚑' })
+sign({ name = 'DiagnosticSignInfo', text = '»' })
+
+-- display diagnostics for LSP errors
+vim.diagnostic.config({
+  virtual_text = false, -- change this to enable inline errors
+  severity_sort = true,
+  float = {
+    border = 'rounded',
+    source = 'always',
+  },
+})
