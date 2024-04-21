3b0258c346ef03a2aa2b50c1b4c2eb72faddfa12
RolandWarburton <warburtonroland@gmail.com>
2024-04-21 22:24:52 +1000

    Clean up nvim-cmp

---
+++ lua/extensions/completion/cmp.lua
@@ -0,0 +1,50 @@
+local M = {}
+local cmp = require('cmp')
+
+-- util function for tabbing through nvim-cmp suggestions
+-- taken from https://github.com/GabrieleStulzer/dotfiles/blob/master/.config/nvim/lua/plugins/nvim-cmp.lua
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+M.mapping = {
+  ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+  ['<C-f>'] = cmp.mapping.scroll_docs(4),
+  ['<C-Space>'] = cmp.mapping.complete(),
+  ['<C-e>'] = cmp.mapping.abort(),
+  ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+
+  -- tab to go to next suggestion
+  ["<Tab>"] = cmp.mapping(function(fallback)
+    if cmp.visible() then
+      cmp.select_next_item()
+    elseif has_words_before() then
+      cmp.complete()
+    else
+      fallback() -- The fallback function sends an already mapped key. In this case, it's probably `<Tab>`.
+    end
+  end, { "i", "s" }),
+
+  -- shift tab to go to previous change
+  ["<S-Tab>"] = cmp.mapping(function()
+    if cmp.visible() then
+      cmp.select_prev_item()
+    end
+  end, { "i", "s" }),
+}
+
+M.sources = {
+  { name = 'nvim_lsp',                max_item_count = 48 },  -- from the language server
+  { name = 'luasnip',                 max_item_count = 128 }, -- from snippets
+  { name = 'buffer' },                                        -- from keywords in the buffer
+  { name = 'nvim_lsp_signature_help', max_item_count = 16 },  -- enables showing signature help
+  { name = 'path',                    max_item_count = 16 },  -- completing file paths
+}
+
+M.window = {
+  completion = cmp.config.window.bordered(),
+  documentation = cmp.config.window.bordered(),
+}
+
+return M
