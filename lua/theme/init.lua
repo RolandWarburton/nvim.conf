6d7a19ae3f1a7123b9facd765df752b5e4b1c94a
RolandWarburton <warburtonroland@gmail.com>
2024-05-15 22:49:45 +1000

    Start laying groundwork for theme switching

--- lua/theme/init.lua
+++ lua/theme/init.lua
@@ -1,19 +1,36 @@
 local M = {}
 
 function M.setup()
+  local theme = 'dark'
+  local theme_opts = {}
+
+  -- set light or dark specific config
+  if theme == 'light' then
+    theme_opts = {
+      transparent = true,
+    }
+  else
+    theme_opts = {
+      transparent = false,
+    }
+  end
+
+  local opts = {
+    italic_comments = true,
+    disable_nvimtree_bg = true,
+    -- Override colors (see ./lua/vscode/colors.lua)
+    color_overrides = {
+      vscLineNumber = '#FFFFFF',
+    },
+  }
+
+  opts = vim.tbl_deep_extend("force", opts, theme_opts)
+
   -- color scheme
-  vim.o.background = 'dark'
+  vim.o.background = theme
   local ok, vscode = pcall(require, 'vscode')
   if ok then
-    vscode.setup({
-      transparent = false,
-      italic_comments = true,
-      disable_nvimtree_bg = true,
-      -- Override colors (see ./lua/vscode/colors.lua)
-      color_overrides = {
-        vscLineNumber = '#FFFFFF',
-      },
-    })
+    vscode.setup(opts)
     require('vscode').load()
   end
 end
