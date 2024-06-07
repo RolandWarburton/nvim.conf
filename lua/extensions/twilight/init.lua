4192d6943e4a081c1d53063d2d54cbe5ec0afa4d
RolandWarburton <warburtonroland@gmail.com>
2024-06-07 18:58:59 +1000

    Update twilight to hl entire function (sorta)
    See https://github.com/folke/twilight.nvim/issues/28

--- lua/extensions/twilight/init.lua
+++ lua/extensions/twilight/init.lua
@@ -3,6 +3,13 @@ local M = {}
 function M.setup()
   require('twilight').setup({
     treesitter = true,
+    context = 1,
+    expand = { -- for treesitter, we we always try to expand to the top-most ancestor with these types
+      "function_declaration",
+      "method_declaration",
+      "method_signature",
+      "ambient_declaration",
+    },
     exclude = { 'md' }
   })
 end
