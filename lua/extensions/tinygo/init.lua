de14888b1c9c03441e9e9981c8cdfcfdc0b24616
RolandWarburton <warburtonroland@gmail.com>
2024-09-17 22:10:46 +1000

    Only init tinygo if running as non privileged user

--- lua/extensions/tinygo/init.lua
+++ lua/extensions/tinygo/init.lua
@@ -3,9 +3,13 @@ local M = {}
 function M.setup()
   require("tinygo").setup()
 
-  -- at the current time i only own pico boards
-  -- you can update this for your board (see :TinyGoTargets)
-  vim.cmd("TinyGoSetTarget pico")
+
+  local user_id = vim.loop.getuid()
+  if user_id and user_id >= 1000 then
+    -- at the current time i only own pico boards
+    -- you can update this for your board (see :TinyGoTargets)
+    vim.cmd("TinyGoSetTarget pico")
+  end
 end
 
 return M
