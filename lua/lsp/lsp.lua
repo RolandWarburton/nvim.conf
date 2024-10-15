0a5fec9141f9ecc5485d4559aeb87a1d0d0edb8e
RolandWarburton <warburtonroland@gmail.com>
2024-10-16 08:29:07 +1100

    Update lua_ls lsp config

--- lua/lsp/lsp.lua
+++ lua/lsp/lsp.lua
@@ -60,27 +60,49 @@ require 'lspconfig'.html.setup {
 }
 
 -- LUA
-USER = vim.fn.expand('$USER')
 require 'lspconfig'.lua_ls.setup {
-  settings = {
-    Lua = {
+  on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
+        return
+      end
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
       runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
         version = 'LuaJIT',
+        path = {
+          "./?.lua",                                         -- Current directory
+          "/usr/local/share/lua/5.1/?.lua",                  -- Local share
+          "/usr/local/share/lua/5.1/?/init.lua",             -- Local share init
+          "/usr/local/lib/lua/5.1/?.lua",                    -- Local lib
+          "/usr/local/lib/lua/5.1/?/init.lua",               -- Local lib init
+          "/usr/share/lua/5.1/?.lua",                        -- System share
+          "/usr/share/lua/5.1/?/init.lua",                   -- System share init
+          "/home/roland/.luarocks/share/lua/5.1/?.lua",      -- Luarocks for user
+          "/home/roland/.luarocks/share/lua/5.1/?/init.lua", -- Luarocks user init
+          "/home/roland/.luarocks/share/lua/5.1/script-utils/init.lua",
+        }
       },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = { 'vim' },
+      cmd = {
+        "/home/roland/.local/bin/lua-language-server",
       },
+      -- Make the server aware of Neovim runtime files
       workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      telemetry = {
-        enable = true,
-      },
-    },
-  },
+        checkThirdParty = true,
+        library = {
+          vim.env.VIMRUNTIME,
+          "/home/roland/.luarocks/share/lua/5.1"
+        },
+      }
+    })
+  end,
+  settings = {
+    Lua = {}
+  }
 }
 
 -- gutter signs for LSP errors
