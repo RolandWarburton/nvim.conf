b8b086878ba040996fd41908ebf7769502dac4b9
RolandWarburton <warburtonroland@gmail.com>
2025-01-05 16:42:04 +1100

    Set fold level

--- lua/core/settings/folds.lua
+++ lua/core/settings/folds.lua
@@ -4,29 +4,34 @@ function M.setup()
   -- tree-sitter based folding
   vim.opt.foldmethod = "expr"
   vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+  vim.opt.foldlevel = 99
 
-  -- if a buffer has been opened for the first time its folds will be expanded
-  local already_loaded_buffers = {}
-
-  -- set up folds to persist through restarts
+    -- set up folds to persist through restarts
   -- neovim will create a "view" for a buffer when closing it, then try to load it when you oad the same buffer
   -- https://github.com/AstroNvim/AstroNvim/blob/271c9c3f71c2e315cb16c31276dec81ddca6a5a6/lua/astronvim/autocmds.lua#L98-L120
   local autocmd = vim.api.nvim_create_autocmd
   local augroup = vim.api.nvim_create_augroup
   local view_group = augroup("auto_view", { clear = true })
 
-    autocmd("BufWinEnter", {
-    desc = "unfold all folds when a buffer is opened",
+  autocmd({ "BufWinLeave", "BufWritePost", "WinLeave" }, {
+    desc = "Save view with mkview for real files",
     group = view_group,
     callback = function(args)
-      local buf = args.buf  -- Get the buffer from args
-      if not vim.b[buf].view_activated then
-        local filetype = vim.api.nvim_get_option_value("filetype", { buf = buf })
-        local buftype = vim.api.nvim_get_option_value("buftype", { buf = buf })
-        local ignore_filetypes = { "gitcommit", "gitrebase", "svg", "hgcommit", "on" }
-        if buftype == "" and filetype and filetype ~= "" and not vim.tbl_contains(ignore_filetypes, filetype) and already_loaded_buffers[buf] == nil then
-          already_loaded_buffers[buf] = true
-          vim.cmd("normal! zR")
+      if vim.b[args.buf].view_activated then vim.cmd.mkview { mods = { emsg_silent = true } } end
+    end,
+  })
+
+  autocmd("BufWinEnter", {
+    desc = "Try to load file view if available and enable view saving for real files",
+    group = view_group,
+    callback = function(args)
+      if not vim.b[args.buf].view_activated then
+        local filetype = vim.api.nvim_get_option_value("filetype", { buf = args.buf })
+        local buftype = vim.api.nvim_get_option_value("buftype", { buf = args.buf })
+        local ignore_filetypes = { "gitcommit", "gitrebase", "svg", "hgcommit" }
+        if buftype == "" and filetype and filetype ~= "" and not vim.tbl_contains(ignore_filetypes, filetype) then
+          vim.b[args.buf].view_activated = true
+          vim.cmd.loadview { mods = { emsg_silent = true } }
         end
       end
     end,
