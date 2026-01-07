bd454859282a53ab468fac2ffbab81169847ea9d
RolandWarburton <warburtonroland@gmail.com>
2026-01-07 18:09:11 +1100

    Move old plugins to bottom of file

--- lua/extensions/init.lua
+++ lua/extensions/init.lua
@@ -4,11 +4,9 @@ require('extensions.telescope').setup()
 require('extensions.surround').setup()
 require('extensions.mason').setup()
 require('extensions.nvimTmuxNavigation').setup()
---require('extensions.null-ls').setup() -- depricated
 require('extensions.neogit').setup()
 require('extensions.neoclip').setup()
 require('extensions.illuminate').setup()
--- require('extensions.hydra').setup() -- disabled due to bug see issue 60
 require('extensions.gitsigns').setup()
 require('extensions.comment-highlight').setup()
 require('extensions.completion').setup()
@@ -20,4 +18,6 @@ require('extensions.colorizer').setup()
 if _G.neovim_config.extensions.enable_tiny_go then
   require('extensions.tinygo').setup()
 end
+--require('extensions.null-ls').setup() -- depricated
+-- require('extensions.hydra').setup() -- disabled due to bug see issue 60
 -- require('extensions.maximize').setup()
