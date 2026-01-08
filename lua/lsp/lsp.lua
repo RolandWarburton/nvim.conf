14a3a09acaba91014941879ff6c38aa8328a3fd5
RolandWarburton <warburtonroland@gmail.com>
2026-01-08 21:09:39 +1100

    re-enable lsp stuff i disabled

--- lua/lsp/lsp.lua
+++ lua/lsp/lsp.lua
@@ -1,29 +1,32 @@
 require 'lsp.keybindings'
 
 -- -- set the debug level for lsp
--- vim.lsp.set_log_level("warn")
+vim.lsp.set_log_level("warn")
+
+local lsp_signature = require('lsp_signature')
+lsp_signature.setup({})
 
 -- feed lsp into the completion engine
--- local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- attach the lsp_signature plugin for param help
--- local on_attach = function(client, bufnr)
---   require('lsp_signature').on_attach({}, bufnr)
--- end
+local on_attach = function(_, bufnr)
+  lsp_signature.on_attach({}, bufnr)
+end
 
 -- display diagnostics for LSP errors
--- vim.diagnostic.config({
---   virtual_text = false, -- change this to enable inline errors
---   severity_sort = true,
---   float = {
---     border = 'rounded',
---     source = 'if_many',
---   },
--- })
+vim.diagnostic.config({
+  virtual_text = false, -- change this to enable inline errors
+  severity_sort = true,
+  float = {
+    border = 'rounded',
+    source = 'if_many',
+  },
+})
 
 vim.lsp.config('*', {
-  -- capabilities = capabilities,
-  -- on_attach = on_attach,
+  capabilities = capabilities,
+  on_attach = on_attach,
   root_markers = { '.git' },
 })
 
