0773d45c04f97a57b5a34df5991ce05838b46809
RolandWarburton <warburtonroland@gmail.com>
2026-01-09 22:59:31 +1100

    Use a custom function for telescope file picker

--- lua/extensions/telescope/init.lua
+++ lua/extensions/telescope/init.lua
@@ -1,5 +1,26 @@
 local M = {}
 
+local function telescope_find_files()
+  local current_file_dir = vim.fn.expand('%:p:h')
+  local cmd = string.format("git -C %s rev-parse --show-toplevel", vim.fn.shellescape(current_file_dir))
+  local git_root = vim.fn.systemlist(cmd)[1]
+
+  local opts = {
+    hidden = true,     -- show hidden files
+    no_ignore = false, -- show .gitignore files
+  }
+
+  -- if the git command failed we are not in a git repo
+  -- so telescope search from the current directory
+  if vim.v.shell_error ~= 0 then
+    opts.cwd = current_file_dir
+    require('telescope.builtin').find_files(opts)
+  else
+    opts.cwd = git_root
+    require('telescope.builtin').find_files(opts)
+  end
+end
+
 function M.setup()
   require('telescope').setup {
     pickers = {
@@ -49,12 +70,7 @@ function M.keybindings()
   Map('n', '<Leader>b', ':Telescope buffers<cr>')
 
   -- map telescope fzf to appropriate ctrl+p to find files
-  local isInGit = os.execute('git rev-parse --is-inside-work-tree')
-  if isInGit == "true" then
-    Map('n', '<C-p>', ':Telescope git_files<cr>')
-  else
-    Map('n', '<C-p>', ':Telescope find_files hidden=true <cr>') -- careful with this on slow computers
-  end
+  Map('n', '<C-p>', function() telescope_find_files() end) -- careful with this on slow computers
 end
 
 return M
