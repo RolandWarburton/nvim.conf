685d1f7f84dd885981498812c49436c28d7d0b5e
RolandWarburton <warburtonroland@gmail.com>
2026-03-04 20:56:38 +1100

    Use typescript-language-server when not using deno

--- ftplugin/typescript.lua
+++ ftplugin/typescript.lua
@@ -1,6 +1,16 @@
-if vim.fn.executable('deno') == 1 then
+local has_package_json = vim.fs.find('package.json', {
+  upward = true,
+  path = vim.fn.expand('%:p:h'),
+  stop = vim.env.HOME,
+})[1] ~= nil
+
+-- use ts_ls (typescript-language-server) if package.json exists
+if has_package_json then
+  vim.lsp.config('ts_ls', {})
+  vim.lsp.enable('ts_ls')
+elseif vim.fn.executable('deno') == 1 then
   vim.lsp.config('denols', {})
   vim.lsp.enable('denols')
 else
-  vim.notify("deno is missing from PATH", vim.log.levels.WARN)
+  vim.notify("No TypeScript LSP available", vim.log.levels.WARN)
 end
