cd8d9cc3f16a0c06f45a860a260fb7e8ca799b23
RolandWarburton <warburtonroland@gmail.com>
2026-05-05 08:53:16 +1000

    Fix old float opts

--- lua/extensions/hydra/init.lua
+++ lua/extensions/hydra/init.lua
@@ -16,7 +16,7 @@ function M.setup()
       color = 'pink',
       invoke_on_body = true,
       hint = {
-        border = 'rounded'
+        float_opts = { border = 'rounded' }
       },
       on_enter = function()
         vim.o.virtualedit = 'all'
